9f7a9af3d12ff2e44a1cd5ab0858751c37f8d32d
firedotguy <nta16022013@gmail.com>
2026-03-04 23:08:47 +0300

    fix(reaquest): remove extra headers

--- itd/request.py
+++ itd/request.py
@@ -13,18 +13,7 @@ def fetch(token: str, method: str, url: str, params: dict = {}, files: dict[str,
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Authorization": 'Bearer ' + token,
-        "Sec-GPC": "1",
-        "Upgrade-Insecure-Requests": "1",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Priority": "u=0, i",
-        "Pragma": "no-cache",
-        "Cache-Control": "no-cache",
-        "TE": "trailers"
+        "Authorization": 'Bearer ' + token
     }
     method = method.lower()
     if method == "get":
@@ -69,24 +58,13 @@ def set_cookies(cookies: str):
 def auth_fetch(cookies: str, method: str, url: str, params: dict = {}, token: str | None = None):
     headers = {
         "Host": "xn--d1ah4a.com",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0",
         "Accept": "*/*",
         "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
         "Referer": "https://xn--d1ah4a.com/",
         "Content-Type": "application/json",
         "Origin": "https://xn--d1ah4a.com",
-        "Sec-GPC": "1",
         "Connection": "keep-alive",
-        "Cookie": cookies,
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-origin",
-        "Priority": "u=4",
-        "Pragma": "no-cache",
-        "Cache-Control": "no-cache",
-        "Content-Length": "0",
-        "TE": "trailers",
+        "Cookie": cookies
     }
     if token:
         headers['Authorization'] = 'Bearer ' + token
